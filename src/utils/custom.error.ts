b9b84b52e5753c40340809c3d011592e41fa8e14
Lucas <flucasyoel@gmail.com>
2024-08-01 14:56:07 -0300

    feat: :sparkles: Class throw Error
    Class Custom Error from throw error for client and for developer

---
+++ src/utils/custom.error.ts
@@ -0,0 +1,17 @@
+interface ICustomError extends Error {
+  clientMessage: string;
+  functionName?: string;
+}
+
+class CustomError extends Error implements ICustomError {
+  clientMessage: string;
+  functionName?: string;
+
+  constructor(message: string, clientMessage: string, functionName?: string) {
+    super(message);
+    this.name = this.constructor.name;
+    this.clientMessage = clientMessage || "Ocurrio un error.";
+    this.functionName = functionName || "Unknown Function";
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
