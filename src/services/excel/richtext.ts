19185d3176c2668daf943b9d836f9a62e1929403
Lucas <flucasyoel@gmail.com>
2024-08-02 00:05:23 -0300

    refactor: :recycle:

--- src/services/excel/richtext.ts
+++ src/services/excel/richtext.ts
@@ -3,25 +3,21 @@ import { CellValue, RichText } from "exceljs";
 export const extractPlainText = (cellValue: any): string => {
   const httpRegex = /https?:\/\/\S*/;
 
-  let text = extractText(cellValue);
-  const match = text.match(httpRegex);
-  return match ? match[0] : "";
-};
-
-const extractText = (cellValue: CellValue | any): string => {
   if (typeof cellValue === "string") {
     return cellValue;
   }
-
   if (isRichText(cellValue)) {
-    return cellValue.richText.map((part: RichText) => part.text).join("");
+    const text = cellValue.richText.map((part: RichText) => part.text).join("");
+
+    const match = text.match(httpRegex);
+
+    return match ? match[0] : "";
   }
 
-  return "";
+  return cellValue?.text || "";
 };
 
-const isRichText = (cellValue: CellValue | any): boolean => {
-  return Boolean(
-    cellValue && typeof cellValue === "object" && cellValue.richText
+const isRichText = (cellValue: CellValue): boolean =>
+  Boolean(
+    cellValue && typeof cellValue === "object" && "richText" in cellValue
   );
-};
